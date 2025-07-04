58f5cd22cf66a7ee014ee88901b4abdecd204873
Neil Julian <neilsree08@gmail.com>
2025-07-04 12:26:31 -0400

    Used proper file paths

--- logging_config.py
+++ logging_config.py
@@ -5,7 +5,9 @@ import os
 def setup_logging():
     logging.basicConfig(
         level=logging.INFO,
-        format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-        datefmt='%Y-%m-%d %H:%M:%S',
-        filename=os.path.abspath('fileOrganizer.log')
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        datefmt="%Y-%m-%d %H:%M:%S",
+        filename=os.path.abspath(
+            os.path.join(os.path.dirname(__file__), "fileOrganizer.log")
+        ),
     )
